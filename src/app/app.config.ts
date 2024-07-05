72aa78c25b748432c474e9a9ef1927921ecfaf9d
Rubesh Chandar <rubeshchander.rc@gmail.com>
2024-07-05 22:50:01 +0100

    Checker emu

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { firebaseConfig } from '../environment';
+import { environment } from '../environments/environment';
+import { USE_EMULATOR as USE_AUTH_EMULATOR } from '@angular/fire/compat/auth';
+import { USE_EMULATOR as USE_FIRESTORE_EMULATOR } from '@angular/fire/compat/firestore';
+import { USE_EMULATOR as USE_FUNCTIONS_EMULATOR } from '@angular/fire/compat/functions';
 
 import { routes } from './app.routes';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+
+console.log("Use emu : " + environment.useEmulators)
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    { provide: FIREBASE_OPTIONS, useValue: firebaseConfig }
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig },
+    { provide: USE_AUTH_EMULATOR, useValue: environment.useEmulators ? ['http://localhost', 9099] : undefined },
+    { provide: USE_FIRESTORE_EMULATOR, useValue: environment.useEmulators ? ['localhost', 8080] : undefined },
+    { provide: USE_FUNCTIONS_EMULATOR, useValue: environment.useEmulators ? ['localhost', 5001] : undefined },
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyALmAeqMtk70-G898uSFKZn9lRMi_qPEnI",
+    authDomain: "home-care-8a1f3.firebaseapp.com",
+    projectId: "home-care-8a1f3",
+    storageBucket: "home-care-8a1f3.appspot.com",
+    messagingSenderId: "312521748553",
+    appId: "1:312521748553:web:f1cb35ef83f05ea2cfcf98",
+    measurementId: "G-RTVZ5Z4N7K"
+  },
+  production: true,
+  useEmulators: false,
+}
