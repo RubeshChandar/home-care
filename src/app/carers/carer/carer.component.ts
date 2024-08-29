9cd2b63cb54613c2f2aea1dee27c0a49a8da8488
Rubesh Chandar <rubeshchander.rc@gmail.com>
2024-08-29 01:44:13 +0100

    Carer and google map are added

--- src/app/carers/carer/carer.component.ts
+++ src/app/carers/carer/carer.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Carer } from '../../models/carer.model';
 import { FirebaseService } from '../../firebase.service';
+import { CommonModule } from '@angular/common';
+import { TimeFormatPipe } from "../../shared/time-format.pipe";
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-carer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TimeFormatPipe],
   templateUrl: './carer.component.html',
   styleUrl: './carer.component.css'
 })
@@ -14,7 +17,12 @@ export class CarerComponent implements OnInit {
   carerID: string = "";
   carer!: Carer;
 
-  constructor(route: ActivatedRoute, private firebaseService: FirebaseService) {
+  constructor(
+    route: ActivatedRoute,
+    private firebaseService: FirebaseService,
+    private sanitizer: DomSanitizer
+  ) {
+    window.scrollTo(0, 0);
     route.params.subscribe(c => this.carerID = c['id'])
   }
 
@@ -22,5 +30,9 @@ export class CarerComponent implements OnInit {
     this.firebaseService.carerSubject.subscribe(carer =>
       this.carer = carer.filter(c => c.id === this.carerID)[0]
     )
+  }
+
+  getLoc() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.google.com/maps?q=${this.carer.location.latitude},${this.carer.location.longitude}&hl=es;z=0&output=embed`);
   }
 }
